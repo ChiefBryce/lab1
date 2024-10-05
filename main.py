e7bb633a4f4cc8c706f1a0a6c062fd0d8a1eb721
Anufriev <anufrievd2005@gmail.com>
2024-10-05 15:13:52 +0900

    initial commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
+
 if __name__ == ‘__main__’:
-	print(‘hello’)
+	print(‘hello!’)
